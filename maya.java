eca06fcfdd5595471bd1c17636185b4982943c8c
Maya-Mel <mayamelamed20@gmail.com>
2026-04-20 12:52:55 +0300

    Initial commit for Exercise 3

---
+++ maya.java
@@ -0,0 +1,7 @@
+public class maya {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Maya - Iteration " + i);
+        }
+    }
+}
